31793159c2bf43fcd991b444cb2ecc2b43b2de98
Ibrahim Yasin Goktas <iygoktas1@gmail.com>
2026-08-21 01:27:03 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
       char characters[2048];
       printf("$ ");
       fgets(characters, sizeof(characters), stdin);
-      if (strcmp(characters, "exit"))
+      if (strcmp(characters, "exit") == 0)
       {
         break;
       }
