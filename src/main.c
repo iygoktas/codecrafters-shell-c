afc99743fd88250577ff084b1466662707a08193
Ibrahim Yasin Goktas <iygoktas1@gmail.com>
2026-08-21 00:49:06 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
+
+    char characters[2048];
+    
+    printf("$ ");
+    fgets(characters, sizeof(characters), stdin);
+    
   // Flush after every printf
-  setbuf(stdout, NULL);
-
+    setbuf(stdout, NULL);
+    characters[strlen(characters) - 1] = '\0';
   // TODO: Uncomment the code below to pass the first stage
-   printf("$ ");
-
+   printf("%s: command not found", characters);
   return 0;
 }
